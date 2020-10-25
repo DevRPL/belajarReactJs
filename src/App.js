6063d092d0193c5ac1477e2b6378b2ee080e1f93
Muhamad Jamil <muhamadjamil668@gmail.com>
2020-10-25 20:36:54 +0700

    StateLess Component & StateFul Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import StateLessComponent from './container/StateLessComponent.jsx';
+import StateFullComponent from './component/StateFullComponent.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+		<StateFullComponent />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		<StateLessComponent />
       </header>
     </div>
   );
---
+++ src/container/StateLessComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StateLessComponent extends Component {
+  render = () => {
+    return (
+      <div>ini adalah StateLessComponent</div>
+    )
+  }
+}
+
+export default StateLessComponent;
