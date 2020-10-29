fa52bc54785c3def898f0bcd2f113fd288945eca
Muhamad Jamil <muhamadjamil668@gmail.com>
2020-10-29 12:48:52 +0700

    method post api

--- src/container/Api/BLogPost/BLogPost.jsx
+++ src/container/Api/BLogPost/BLogPost.jsx
@@ -5,7 +5,12 @@ import axios from 'axios';
 class BLogPost extends Component {
 
 	state = {
-		post: []
+		post: [],
+		fromBLogPost: {
+			title: '',
+			body: '',
+			description: '',
+		}
 	}
 	componentDidMount() {
 		/*
@@ -44,13 +49,42 @@ class BLogPost extends Component {
 		axios({
 			method: 'get',
 			url: 'http://localhost:8000/post',
-			responseType: 'json'
+			responseType: 'json',
 		})
 		.then(json => {
 			this.setState({
 				post:json.data
 			})
 		})
+	}
+
+	postDataToAPi = () => {
+		console.log(this.state.fromBLogPost);
+		axios({
+			method: 'post',
+			url: 'http://localhost:8000/post/store',
+			data: this.state.fromBLogPost,
+			responseType: 'json'
+		})
+		.then(json => {
+			this.getPostAPi(json);
+		}, (err) => {
+			console.log(err);
+		})
+	}
+
+	handleFormChange = (evt) => {
+		let fromBLogPostNew = {...this.state.fromBLogPost};
+		fromBLogPostNew[evt.target.name] = evt.target.value;
+		this.setState({
+			fromBLogPost: fromBLogPostNew
+		}, () => {
+			console.log(this.state.fromBLogPost)
+		})
+	}
+
+	handleSubmit = () => {
+		this.postDataToAPi();
 	}
 
 	handleRemove = (data) => {
@@ -67,6 +101,14 @@ class BLogPost extends Component {
   render = () => {
     return (
 		<Fragment>
+
+		<div className="form-add-post">
+			<input type="text" name="title" onChange={this.handleFormChange}/><br></br><br></br> 
+			<input type="text" name="body" onChange={this.handleFormChange}/><br></br><br></br> 
+			<textarea name="description" onChange={this.handleFormChange}></textarea> <br></br><br></br>
+			<button onClick={this.handleSubmit}>Simpan</button>
+		</div><br></br> <br></br>
+
 			{
 				this.state.post.map(post => {
 					return <Post key={post.id} data={post} remove={this.handleRemove}/>
