91bb86425d34056f92b7f5bf1273037fa45a9083
Localhost127.0.0.1 <localhost127001@localhost127.0.0.1>
2020-11-28 18:50:47 +0700

    Redux (Dispatch & Subscription)

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -3,18 +3,49 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
+    status: '',
     value: 0, 
-    age: 22
+    qty: 22
 }
 
 // REDUCER
 
 const rootReducer = (state = initialState, action) => {
-    return state;
+    if(action.value > 0) {
+        if (action.type === 'NEW') {
+            return {
+                ...state,
+                status: 'PROCESS',
+                qty: state.qty + action.value,
+                value: action.value
+            }
+        } else if(action.type === 'PROCESS') {
+            return {
+                ...state,
+                status: 'DONE',
+                qty: state.qty + action.value,
+                value: action.value
+            }
+        } 
+    }
+
+    return state;   
 }
 
 // STORE
 
 const store = createStore(rootReducer);
+
+// SUBSCRIPTION
+
+store.subscribe(() => { 
+    console.log("store change", store.getState())
+})
+
+// DISPATCHING
+
+store.dispatch({type: 'NEW', value:1})
+
+
 
 console.log(store.getState())
